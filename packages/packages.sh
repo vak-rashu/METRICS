3924abed888032059fba5d9165a18c4e06eba1d3
vak-rashu <rashu.cloud11@gmail.com>
2025-04-10 22:58:22 +0530

    Create packages.sh

---
+++ packages/packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VENV_NAME="metrics_env"
+
+if [ ! -d "$VENV_NAME" ]; then
+  echo " Creating virtual environment..."
+  python3 -m venv "$VENV_NAME"
+fi
+
+# Activate the venv
+source "$VENV_NAME/bin/activate"
+
+echo " Installing packages..."
+pip install --upgrade pip
+pip install psutil matplotlib prettytable
+
+echo "Installation complete."
+
